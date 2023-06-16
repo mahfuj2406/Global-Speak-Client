e700544649083ebf023f26b1038fd1532fa9304a
Md. Mahfujur Rahman <mahfujur15-2406@diu.edu.bd>
2023-06-16 15:49:15 +0600

    error page updated

--- src/pages/Shared/ErrorPage/ErrorPage.jsx
+++ src/pages/Shared/ErrorPage/ErrorPage.jsx
@@ -9,7 +9,7 @@ const ErrorPage = () => {
             <p className="text-gray-500 text-center mb-8">
                 We're sorry, but the page you requested could not be found.
             </p>
-            <Link to="/" className="bg-teal-800 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded-full">
+            <Link to="/" className="bg-teal-800 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full">
                 Go back to homepage
             </Link>
         </div>
